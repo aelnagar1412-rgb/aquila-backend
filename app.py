a918edb1e3215d30223251c136e049760e4c1d57
aelnagar1412-rgb <aelnagar1412@gmail.com>
2025-12-24 18:18:45 +0200

    Update app.py

--- app.py
+++ app.py
@@ -1,16 +1,17 @@
 from flask import Flask
+from flask_cors import CORS
 from routes.status import status_bp
 
 def create_app():
     app = Flask(__name__)
+    CORS(app)
 
     # Register Blueprints
     app.register_blueprint(status_bp)
 
     return app
 
-
 app = create_app()
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000, debug=True)
+    app.run(host="0.0.0.0", port=5000)
